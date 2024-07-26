08c46904719f51f3efc620be89c4edca4e29702c
Firoj Mahmud Shuvho <mahmudshuvho111@gmail.com>
2024-07-26 18:13:20 +0600

    initial commit

---
+++ Dart Basics/variables.dart
@@ -0,0 +1,36 @@
+
+/*Variables are containers used to store value in the program. There are different types of variables where you can keep different kinds of values.
+String: For storing text value. E.g. “John” [Must be in quotes]
+int: For storing integer value. E.g. 10, -10, 8555 [Decimal is not included]
+double: For storing floating point values. E.g. 10.0, -10.2, 85.698 [Decimal is included]
+num: For storing any type of number. E.g. 10, 20.2, -20 [both int and double]
+bool: For storing true or false. E.g. true, false [Only stores true or false values]
+var: For storing any value. E.g. ‘Bimal’, 12, ‘z’, true*/
+
+void main() {
+// declaring variables
+String name = "Fiorj Mahmud";
+int age = 25; // used to store any types of numbers 
+double height = 5.7;
+num roll = 02;
+bool isMarried = true;
+var salary = 22000;
+   
+// printing variables value   
+print("Name is $name");
+print("Age is $age");
+print("Height is $height");
+print("Roll is $roll");
+print("Married Status is $isMarried");
+print("Salary is $salary");
+}
+
+//Output
+/*
+Name is Fiorj Mahmud
+Age is 25
+Height is 5.7
+Roll is 2
+Married Status is true
+Salary is 22000
+*/
