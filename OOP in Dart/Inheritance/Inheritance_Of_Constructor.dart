557ea61d903b4f750f87dd3a0883c0d0f83c071a
Firoj Mahmud Shuvho <mahmudshuvho111@gmail.com>
2024-07-27 17:03:49 +0600

    Example of Inheritance

---
+++ OOP in Dart/Inheritance/Inheritance_Of_Constructor.dart
@@ -0,0 +1,23 @@
+class Person {
+  String name;
+  int age;
+
+  // Constructor
+  Person(this.name, this.age);
+}
+
+class Student extends Person {
+  int rollNumber;
+
+  // Constructor
+  Student(String name, int age, this.rollNumber) : super(name, age); //Super is used to refer to the parent class. 
+                                                                      //It is used to call the parent class’s properties and methods.
+}
+
+void main() {
+  var student = Student("John", 20, 1);
+  print("Student name: ${student.name}");
+  print("Student age: ${student.age}");
+  print("Student roll number: ${student.rollNumber}");
+
+}
