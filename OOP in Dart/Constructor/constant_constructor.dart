82014e92df7189f76735ef892450608f6173c4f4
Firoj Mahmud Shuvho <mahmudshuvho111@gmail.com>
2024-07-27 14:27:49 +0600

    Constructor Types and examples

--- OOP in Dart/Constructor/constant_constructor.dart
+++ OOP in Dart/Constructor/constant_constructor.dart
@@ -12,15 +12,15 @@ class Point {
 void main() {
   // p1 and p2 has the same hash code.
   Point p1 = const Point(1, 2);
-  print("The p1 hash code is: ${p1.hashCode}");
+  print("The p1 hash code is: ${p1.hashCode}"); //The p1 hash code is: 434505566
 
   Point p2 = const Point(1, 2);
-  print("The p2 hash code is: ${p2.hashCode}");
+  print("The p2 hash code is: ${p2.hashCode}"); //The p1 hash code is: 434505566
   // without using const
   // this has different hash code.
   Point p3 = Point(2, 2);
-  print("The p3 hash code is: ${p3.hashCode}");
+  print("The p3 hash code is: ${p3.hashCode}"); //The p3 hash code is: 272159569
 
   Point p4 = Point(2, 2);
-  print("The p4 hash code is: ${p4.hashCode}");
+  print("The p4 hash code is: ${p4.hashCode}"); //The p4 hash code is: 269480714
 }
